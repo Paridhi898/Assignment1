b99982068080de961c5aa6ca426871798c9905e4
paridhi898 <parirl898@gmail.com>
2025-09-04 09:24:31 +0545

    script

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+// intro.php
+
+// Store information in variables
+$name = "Paridhi Rana";
+$age = 21;
+$program = "BCSIT";
+
+// Display the message
+echo "My name is $name, I am $age years old, and I study $program.";
+?>
